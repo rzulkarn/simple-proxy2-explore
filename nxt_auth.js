b9dbf20ec2d66e101947f43003f93dcafa919cc3
36993074+rzulkarn@users.noreply.github.com <rzulkarn@yahoo.com>
2019-03-02 15:13:20 -0800

    Add nxt_auth.js

---
+++ nxt_auth.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const router = express.Router()
+
+const todosByUser = {}
+
+router.post('/', (req, res, next) => {
+  const todo = [...req.body.todo || []]
+  if (req.body.remove) todo.splice(req.body.remove, 1)
+  if (req.body.new) todo.push({})
+
+  todosByUser[req.userContext.userinfo.sub] = todo
+
+  next()
+})
+
+router.use('/', (req, res) => {
+  const todo = todosByUser[req.userContext.userinfo.sub] || []
+
+  res.json({ title: 'To-do list', todo, userinfo: req.userContext.userinfo });
+})
+
+module.exports = router
